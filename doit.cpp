379ffc8977e7e46a8f346f064eb18febf095292a
Arthur Lockman <hello@rthr.me>
2015-08-29 19:19:18 -0400

    Doit now can exec processes.

--- doit.cpp
+++ doit.cpp
@@ -5,7 +5,6 @@
  * @author Arthur Lockman <ajlockman@wpi.edu>
  */
 
-
 #include <iostream>
 #include <unistd.h>
 #include <sys/wait.h>
@@ -24,10 +23,33 @@ int main(int argc, char **argv)
     {
         cout << "Executing as shell..." << endl;
         //Exec as shell
-    } else if (argc > 1)
+    }
+    else if (argc > 1)
     {
+        int pid;
         cout << "Executing command..." << endl;
         //Exec given command
+        if ((pid = fork()) < 0) //fork failed
+        {
+            cerr << "Fork error!" << endl;
+        }
+        else if (pid == 0)   //is child
+        {
+            char *argvNew[argc];
+            for (int i = 1; i < argc; i++)
+            {
+                argvNew[i - 1] = argv[i];
+            }
+            argvNew[argc - 1] = NULL;
+            if (execvp(argvNew[0], argvNew) < 0)
+            {
+                cerr << "Execvp error!" << endl;
+                exit(1);
+            }
+        }
+        else //is parent
+        {
+        }
     }
 }
 
